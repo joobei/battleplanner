4a009d97817658b18fdf3744e1e7ef5769ad7f33
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-02-19 19:18:44 +0100

    Read match data from JSON

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <h1>Hell Let Loose War Planner</h1>
+    <!-- <Upcoming v-bind:match_data="matches" /> -->
+            <div v-for="data in myJson.matches" v-bind:key="data.id">
+              {{data.id}}
+              </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import Upcoming from './components/Upcoming';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import json from './assets/matches.json'
+
+export default{
+          data(){
+              return{
+                  myJson: json
+              }
+          }
+      }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  * {
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+  }
+
+  body {
+    background-color: rgb(17,17,17);
+  font-family: Inter, Roboto, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  line-height: 1.4;
+  }
 </style>
---
+++ src/components/Match.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Upcoming Matches</h1>
+        <div v-for="match in match_data" :key="match.id">
+            <h3>{{match.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Upcoming",
+    props:["match_data"]
+}
+</script>
+
+<style scoped>
+</style>
